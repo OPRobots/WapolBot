0b2db1f7a9ab2bbea87b5245e21a27001320b93e
Hunter D Ace <danikenshin_93@hotmail.com>
2024-08-19 20:30:11 +0200

    peoras robolid 24

--- source_code/include/estrategias.h
+++ source_code/include/estrategias.h
@@ -2,11 +2,15 @@
 #define ESTRATEGIAS_H
 #include <Arduino.h>
 
+extern bool near;
+
 void estrategiaMirarAtras();
 void estrategiaMirarLadoDerecha();
 void estrategiaMirarLadoIzquierda();
 void estrategiaCaja();
 void estrategiaRadar();
 void estrategiaBase();
+void estrategiaSharps();
+void estrategiaQre1113();
 
 #endif
